c418f40dbccd3249ef8493746c28562898471abc
daancover <daniel.salgado@zup.com.br>
2018-08-20 16:28:29 -0300

    Firestore add

--- app/src/main/java/com/coverlabs/mvvmdagger/view/MainActivity.kt
+++ app/src/main/java/com/coverlabs/mvvmdagger/view/MainActivity.kt
@@ -2,11 +2,13 @@ package com.coverlabs.mvvmdagger.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.coverlabs.mvvmdagger.R
 import com.coverlabs.mvvmdagger.di.DaggerMVVMDaggerComponent
 import com.coverlabs.mvvmdagger.model.User
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
     @Inject
@@ -16,6 +18,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DaggerMVVMDaggerComponent.create().inject(this)
-        tvName.text = "${mUser.name} ${mUser.age}"
+
+        val db = FirebaseFirestore.getInstance()
+
+        val user = HashMap<String, Any>()
+        user["name"] = mUser.name
+        user["age"] = mUser.age
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference -> Log.d("MainActivity", "DocumentSnapshot added with ID: " + documentReference.id) }
+                .addOnFailureListener { e -> Log.w("MainActivity", "Error adding document", e) }
     }
 }
